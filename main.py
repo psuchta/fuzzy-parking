951c810d86d03ae463cc7aa876570ff5c0e76caf
Pawel Suchta <pawel.suchta@polcode.net>
2021-02-09 01:37:07 +0100

    Add method for adding car to the game

--- main.py
+++ main.py
@@ -36,30 +36,39 @@ class Game:
 
 	def __add_game_objects(self):
 		self.all_sprites = pygame.sprite.Group()
+		self.cars = []
 		self.walls = pygame.sprite.Group()
-		self.car = AutonomousCar(600, 240 + 64)
-		# self.car = ControllerCar(200, 140 + 64)
-		self.all_sprites.add(self.car)
+		self.add_car(AutonomousCar(600, 240 + 64))
+		self.add_car(ControllerCar(400, 140 + 64))
 		# Create walls starting from 0 (x) 140 (y)
 		# Center of the parking slot is 40(x)
 		# Slot width is 80 px
 		# Slot height is 140 px
-		self.parking_slot = ParkingSlot(0, 140, 40, 80, 140)
+		self.parking_slot = ParkingSlot(0, 140, 140, 80, 140)
 		for w in self.parking_slot.walls:
 			self.all_sprites.add(w)
 			self.walls.add(w)
 
-		self.car.walls = self.walls
+	def add_car(self, car):
+		self.cars.append(car)
+		self.all_sprites.add(car)
+		car.walls = self.all_sprites
+
+	def update_cars(self, cars, dt, parking_slot):
+		for car in cars:
+			if isinstance(car, AutonomousCar): 
+				car.autonomouse_steering(dt, parking_slot)
+			else:
+				car.update(dt)
+
 
 	def run(self):
-		car = self.car
 		while not self.exit:
 			dt = self.clock.get_time() / 1000
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					self.exit = True			
-			# self.car.update(dt)
-			self.car.autonomouse_steering(dt, self.parking_slot.slot_x + self.parking_slot.slot_width/2, self.parking_slot.slot_height)
+			self.update_cars(self.cars, dt, self.parking_slot)
 			self.draw_objects()
 			self.clock.tick(self.fps)
 		pygame.quit()
